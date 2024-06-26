a3d6f500db2ee04103f7e863a979599e18fcae97
Yogesh Pathak <91154219+WebYogesh@users.noreply.github.com>
2024-06-26 08:21:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+let name = "yogesh" //string
+let age = 20 //number
+let isLoogedIn = false // Boolean
+
+// number => 2 to power 53
+//string => ""
+// Boolean => true/false
+//null => stantAlone Value
+//undifined => 
+//symbol => Unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
