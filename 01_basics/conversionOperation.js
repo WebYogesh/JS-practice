13b2ada39eb3593aa48e54bd7d1ca3d5c61cb318
Yogesh Pathak <91154219+WebYogesh@users.noreply.github.com>
2024-06-26 10:00:57 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -23,6 +23,46 @@ let booleanIsLogedIn = Boolean(isLogedIn)
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+//*********************OPERATIONS************************
+
+let value = 3
+let nagative = -value
+// console.log(nagative);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Yogesh"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + (2 + 2));
+// console.log((1 + 2) + "2");
+
+console.log(+(true));
+console.log(+"");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
