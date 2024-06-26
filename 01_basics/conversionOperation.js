0b8b6a726b14270c0e7aa982d9d72bc1ce7fe33b
Yogesh Pathak <91154219+WebYogesh@users.noreply.github.com>
2024-06-26 09:41:01 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "53"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0
+
+
+let isLogedIn = 1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+//console.log(booleanIsLogedIn)
+
+// 1 => true; 0 => false
+//"" => false
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+
